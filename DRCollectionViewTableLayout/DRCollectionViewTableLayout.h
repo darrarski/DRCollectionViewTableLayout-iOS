b711bb710b8031a4a3b55437f0664a0f602814e1
Darrarski <darrarski@gmail.com>
2014-05-09 03:04:43 +0200

    Added documentation comments to header files

--- DRCollectionViewTableLayout/DRCollectionViewTableLayout.h
+++ DRCollectionViewTableLayout/DRCollectionViewTableLayout.h
@@ -10,21 +10,88 @@
 
 @class DRCollectionViewTableLayout;
 
+/**
+ *  Collection View Table Layout Delegate
+ */
 @protocol DRCollectionViewTableLayoutDelegate <NSObject>
 
+/**
+ *  Return number of columns in given section
+ *
+ *  @param collectionView       Collection View
+ *  @param collectionViewLayout Collection View Layout
+ *  @param section              Section index (from collection view cell's indexPath property)
+ *
+ *  @return Number of columns
+ */
 - (NSUInteger)collectionView:(UICollectionView *)collectionView tableLayout:(DRCollectionViewTableLayout *)collectionViewLayout numberOfColumnsInSection:(NSUInteger)section;
+
+/**
+ *  Return width for column in given section
+ *
+ *  @param collectionView       Collection View
+ *  @param collectionViewLayout Collection View Layout
+ *  @param column               Layout's column index
+ *  @param section              Section index (from collection view cell's indexPath property)
+ *
+ *  @return Column width
+ */
 - (CGFloat)collectionView:(UICollectionView *)collectionView tableLayout:(DRCollectionViewTableLayout *)collectionViewLayout widthForColumn:(NSUInteger)column inSection:(NSUInteger)section;
+
+/**
+ *  Return height for row in given section
+ *
+ *  @param collectionView       Collection View
+ *  @param collectionViewLayout Collection View Layout
+ *  @param row                  Layout's row index
+ *  @param section              Section index (from collection view cell's indexPath property)
+ *
+ *  @return Row height
+ */
 - (CGFloat)collectionView:(UICollectionView *)collectionView tableLayout:(DRCollectionViewTableLayout *)collectionViewLayout heightForRow:(NSUInteger)row inSection:(NSUInteger)section;
 
 @end
 
+/**
+ *  Collection View Table Layout
+ */
 @interface DRCollectionViewTableLayout : UICollectionViewLayout
 
+/**
+ *  Horizontal spacing between cells
+ */
 @property (nonatomic, assign) CGFloat horizontalSpacing;
+
+/**
+ *  Vertical spacing between cells
+ */
 @property (nonatomic, assign) CGFloat verticalSpacing;
 
+/**
+ *  Initialize with delegate
+ *
+ *  @param delegate Collection View Table Layout Delegate
+ *
+ *  @return new Collection View Table Layout
+ */
 - (id)initWithDelegate:(id<DRCollectionViewTableLayoutDelegate>)delegate;
+
+/**
+ *  Return layout's column index for collection view cell's indexPath
+ *
+ *  @param indexPath Collection View Cell's indexPath
+ *
+ *  @return Layout's column index
+ */
 - (NSUInteger)columnNumberForIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ *  Return layout's row index for collection view cell's indexPath
+ *
+ *  @param indexPath Collection View Cell's indexPath
+ *
+ *  @return Layout's row index
+ */
 - (NSUInteger)rowNumberForIndexPath:(NSIndexPath *)indexPath;
 
 @end
--- DRCollectionViewTableLayout/DRCollectionViewTableLayoutManager.h
+++ DRCollectionViewTableLayout/DRCollectionViewTableLayoutManager.h
@@ -11,29 +11,89 @@
 
 @class DRCollectionViewTableLayoutManager;
 
+/**
+ *  Collection View Table Layout Manager Delegate
+ */
 @protocol DRCollectionViewTableLayoutManagerDelegate <NSObject>
 
+/**
+ *  Return number of sections in given collection view
+ *
+ *  @param manager        Collection View Table Layout Manager
+ *  @param collectionView Collection View
+ *
+ *  @return Number of sections
+ */
 - (NSUInteger)collectionViewTableLayoutManager:(DRCollectionViewTableLayoutManager *)manager
               numberOfSectionsInCollectionView:(UICollectionView *)collectionView;
 
+/**
+ *  Return number of layout's columns in given collection view section
+ *
+ *  @param manager        Collection View Table Layout Manager
+ *  @param collectionView Collection View
+ *  @param section        Collection View section
+ *
+ *  @return Number of layout's columns
+ */
 - (NSUInteger)collectionViewTableLayoutManager:(DRCollectionViewTableLayoutManager *)manager
                                 collectionView:(UICollectionView *)collectionView
                       numberOfColumnsInSection:(NSUInteger)section;
 
+/**
+ *  Return number of layout's rows in given collection view section
+ *
+ *  @param manager        Collection View Table Layout Manager
+ *  @param collectionView Collection View
+ *  @param section        Collection View section
+ *
+ *  @return Number of layout's rows
+ */
 - (NSUInteger)collectionViewTableLayoutManager:(DRCollectionViewTableLayoutManager *)manager
                                 collectionView:(UICollectionView *)collectionView
                          numberOfRowsInSection:(NSUInteger)section;
 
+/**
+ *  Return width for layout's column in given collection view section
+ *
+ *  @param manager        Collection View Table Layout Manager
+ *  @param collectionView Collection View
+ *  @param column         Layout's column index
+ *  @param section        Collection View section
+ *
+ *  @return Column width
+ */
 - (CGFloat)collectionViewTableLayoutManager:(DRCollectionViewTableLayoutManager *)manager
                              collectionView:(UICollectionView *)collectionView
                              widthForColumn:(NSUInteger)column
                                   inSection:(NSUInteger)section;
 
+/**
+ *  Return height for layout's row in given collection view section
+ *
+ *  @param manager        Collection View Table Layout Manager
+ *  @param collectionView Collection View
+ *  @param row            Layout's row index
+ *  @param section        Collection View section
+ *
+ *  @return Row height
+ */
 - (CGFloat)collectionViewTableLayoutManager:(DRCollectionViewTableLayoutManager *)manager
                                 collectionView:(UICollectionView *)collectionView
                                heightForRow:(NSUInteger)row
                                   inSection:(NSUInteger)section;
 
+/**
+ *  Return cell for given row and column in collection view
+ *
+ *  @param manager        Collection View Table Layout Manager
+ *  @param collectionView Collection View
+ *  @param row            Layout's row
+ *  @param column         Layout's column
+ *  @param indexPath      Collection View Cell index path
+ *
+ *  @return Collection View Cell
+ */
 - (UICollectionViewCell *)collectionViewTableLayoutManager:(DRCollectionViewTableLayoutManager *)manager
                                             collectionView:(UICollectionView *)collectionView
                                                 cellForRow:(NSUInteger)row
@@ -42,8 +102,14 @@
 
 @end
 
+/**
+ *  Collection View Table Layout Manager
+ */
 @interface DRCollectionViewTableLayoutManager : NSObject <UICollectionViewDataSource, DRCollectionViewTableLayoutDelegate>
 
+/**
+ *  Collection View Table Layout Manager Delegate
+ */
 @property (weak, nonatomic) id<DRCollectionViewTableLayoutManagerDelegate> delegate;
 
 @end
